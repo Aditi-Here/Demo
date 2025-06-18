96734b04d281b034bfc6857f10ce0d32ff88b197
Aditi-Here <66709120+Aditi-Here@users.noreply.github.com>
2025-06-17 23:30:17 -0400

    Add files via upload

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+    @Test
+    public void testName() {
+        Assertions.assertEquals("Yes", "Yes");
+    }
+}
